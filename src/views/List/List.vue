874f26c2f489d3338214a8cea8e7b0bb66b01f6e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-01-18 13:41:40 +0100

    Displaying user place

--- src/views/List/List.vue
+++ src/views/List/List.vue
@@ -1,14 +1,30 @@
 <template lang="pug">
 section
     h1(v-t="'list.title'")
+    h4 Showing garments in: {{currentUserLocation}}
     garment-list
 </template>
 
 <script>
 import GarmentList from "./components/GarmentList";
+import store from "@/currentUser";
 export default {
   components: {
     GarmentList: GarmentList,
+  },
+  data: function () {
+    return {
+      currentUserLocation: "place not set",
+    };
+  },
+  mounted() {
+    this.getCurrentUser();
+  },
+  methods: {
+    async getCurrentUser() {
+      const currentUser = await store.getCurrentUser();
+      this.currentUserLocation = currentUser.place;
+    },
   },
 };
 </script>
