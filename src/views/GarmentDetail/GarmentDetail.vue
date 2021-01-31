8a7b28b68ba1efeee1546b86b4b6ebf57b32dbae
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-01-31 17:27:25 +0100

    Even more refactor

--- src/views/GarmentDetail/GarmentDetail.vue
+++ src/views/GarmentDetail/GarmentDetail.vue
@@ -2,90 +2,22 @@
 section
     h3(v-if='loading' ) Loading...
     div(v-else)
-        img.mb-4.img-fluid(:src="imageUrl")
-        p.mb-0 Id: {{ garment.id}}
-        p.mb-0 {{$t("garmentDetails.name")}}: {{ garment.name }}
-        p.mb-0 {{$t("garmentDetails.garment_type")}}: {{ garment.garment_type}}
-        p.mb-0 {{$t("garmentDetails.color")}}: {{ garment.color}}
-        p.mb-0 {{$t("garmentDetails.status")}}: {{ garment.status}}
-        p.mb-0 {{$t("garmentDetails.place")}}: {{ garment.place}}
-        b-button(variant="danger" v-on:click="onDelete" v-t="'garmentDetails.deleteIt'")
-        router-link(:to="{ name: 'edit', params: { garmentId: garmentId}}")
-            b-button.ml-2(v-t="'garmentCard.editIt'")
-        journal-entry(
-            v-for='entry in journal' :key='entry.id'
-            :message="entry.message"
-            :timestamp="entry.timestamp"
-        )
-    add-journal-entry.mt-4(:garment-id="this.garmentId")
+        garment-card-big(:garment-id="garmentId")
+        journal(:garment-id="garmentId")
 </template>
 
 <script>
-import { getJournal, getGarment, deleteGarment } from "@/apis/apis";
-import { getImageUrl } from "@/apis/helpers";
-import { errorToast, okToast } from "@/helpers/ui";
-import JournalEntry from "./components/JournalEntry";
-import AddJournalEntry from "./components/AddJournalEntry";
-import router from "@/router";
+import Journal from "@/components/Journal";
+import GarmentCardBig from "./components/GarmentCardBig";
 export default {
   components: {
-    JournalEntry: JournalEntry,
-    AddJournalEntry: AddJournalEntry,
+    Journal,
+    GarmentCardBig,
   },
   props: {
     garmentId: {
       type: Number,
       default: 0,
-    },
-  },
-  data: function () {
-    return {
-      journal: null,
-      garment: null,
-      loading: true,
-    };
-  },
-  computed: {
-    imageUrl: function () {
-      if (this.garment.image) {
-        return getImageUrl(this.garment.image);
-      }
-      return null;
-    },
-  },
-  mounted: function () {
-    this.loadJournal();
-    this.loadGarment();
-  },
-  methods: {
-    async loadJournal() {
-      try {
-        this.journal = await getJournal(this.garmentId);
-      } catch (err) {
-        this.$bvToast.toast(`Journal can't be retrieved`, errorToast);
-      } finally {
-        this.loading = false;
-      }
-    },
-    async loadGarment() {
-      try {
-        this.garment = await getGarment(this.garmentId);
-      } catch (err) {
-        this.$bvToast.toast(`Garment can't be retrieved`, errorToast);
-      } finally {
-        this.loading = false;
-      }
-    },
-    async onDelete(evt) {
-      evt.preventDefault();
-      try {
-        deleteGarment(this.garment.id);
-        router.replace("/list", () => {
-          this.$root.$bvToast.toast(`Garment removed`, okToast);
-        });
-      } catch (err) {
-        this.$bvToast.toast(`Garment could not be removed`, errorToast);
-      }
     },
   },
 };
---
+++ src/views/GarmentDetail/components/GarmentCardBig.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+b-spinner(v-if="loading" label="Loading...")
+section(v-else)
+    img.mb-4.img-fluid(:src="imageUrl")
+    p.mb-0 Id: {{ garment.id}}
+    p.mb-0 {{$t("garmentDetails.name")}}: {{ garment.name }}
+    p.mb-0 {{$t("garmentDetails.garment_type")}}: {{ garment.garment_type}}
+    p.mb-0 {{$t("garmentDetails.color")}}: {{ garment.color}}
+    p.mb-0 {{$t("garmentDetails.status")}}: {{ garment.status}}
+    p.mb-0 {{$t("garmentDetails.place")}}: {{ garment.place}}
+    b-button(variant="danger" v-on:click="onDelete" v-t="'garmentDetails.deleteIt'")
+    router-link(:to="{ name: 'edit', params: { garmentId: this.garment.id}}")
+        b-button.ml-2(v-t="'garmentCard.editIt'")
+</template>
+
+<script>
+import { getGarment, deleteGarment } from "@/apis/apis";
+import { getImageUrl } from "@/apis/helpers";
+import { errorToast, okToast } from "@/helpers/ui";
+import router from "@/router";
+export default {
+  props: {
+    garmentId: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data: function () {
+    return {
+      garment: null,
+      loading: true,
+    };
+  },
+  computed: {
+    imageUrl: function () {
+      if (this.garment.image) {
+        return getImageUrl(this.garment.image);
+      }
+      return null;
+    },
+  },
+  watch: {
+    garmentId: {
+      immediate: true,
+      handler: function (newData) {
+        this.loadGarment(newData);
+      },
+    },
+  },
+  methods: {
+    async loadGarment(garmentId) {
+      try {
+        this.garment = await getGarment(garmentId);
+      } catch (err) {
+        this.$bvToast.toast(`Garment can't be retrieved`, errorToast);
+      } finally {
+        this.loading = false;
+      }
+    },
+    async onDelete(evt) {
+      evt.preventDefault();
+      try {
+        deleteGarment(this.garment.id);
+        router.replace("/list", () => {
+          this.$root.$bvToast.toast(`Garment removed`, okToast);
+        });
+      } catch (err) {
+        this.$bvToast.toast(`Garment could not be removed`, errorToast);
+      }
+    },
+  },
+};
+</script>
