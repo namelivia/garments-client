d8a10cfecde503a304917fc59403ddd70aab3de2
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-01-31 14:31:22 +0100

    Even more refractoring

--- src/views/New/New.vue
+++ src/views/New/New.vue
@@ -1,120 +1,29 @@
 <template lang="pug">
 section
     h1 Add new garment
-    b-form(@submit="onSubmit" @reset="onReset"  v-if="show")
-        b-form-group(
-            id="name-input-group"
-            :label="$t('newGarment.name')"
-            label-for="name"
-        )
-            b-form-input(
-                id="name"
-                name="name"
-                v-model="form.name"
-                type="text"
-                required
-                :placeholder="$t('newGarment.enterGarmentName')"
-            )
-        garment-type-selector(@selected="onGarmentTypeSelected")
-        place-selector(@selected="onPlaceSelected")
-        b-form-group(
-            id="color-input-group"
-            :label="$t('newGarment.color')"
-            label-for="color"
-        )
-            b-form-input(
-                id="color"
-                name="color"
-                v-model="form.color"
-                type="text"
-                :placeholder="$t('newGarment.enterGarmentColor')"
-            )
-        b-form-group(
-            id="status-input-group"
-            :label="$t('newGarment.status')"
-            label-for="status"
-        )
-            b-form-input(
-                id="status"
-                name="status"
-                v-model="form.status"
-                type="text"
-                :placeholder="$t('newGarment.enterGarmentStatus')"
-            )
-        resize-image-upload(@loaded="onImageLoaded")
-        .mt-4
-        b-button.mr-2(type="submit" variant="primary") {{$t('newGarment.submit')}}
-        b-button(type="reset" variant="danger") {{$t('newGarment.reset')}}
+    garment-form(@submit="onSubmit")
 </template>
 
 <script>
 import router from "@/router";
-import GarmentTypeSelector from "@/components/GarmentTypeSelector";
-import PlaceSelector from "@/components/PlaceSelector";
-import ResizeImageUpload from "@/components/ResizeImageUpload";
-import { postImage, postGarment } from "@/apis/apis";
+import GarmentForm from "@/components/GarmentForm";
+import { postGarment } from "@/apis/apis";
 import { errorToast, okToast } from "@/helpers/ui";
 export default {
   components: {
-    GarmentTypeSelector: GarmentTypeSelector,
-    PlaceSelector: PlaceSelector,
-    ResizeImageUpload: ResizeImageUpload,
+    GarmentForm,
   },
-  data() {
-    return {
-      form: {
-        name: "",
-        garment_type: "",
-        status: "",
-        color: "",
-        place: "",
-        image: null,
-      },
-      show: true,
-    };
-  },
-  computed: {},
   methods: {
-    onGarmentTypeSelected(selectedGarmentType) {
-      this.form.garment_type = selectedGarmentType;
-    },
-    onPlaceSelected(selectedPlace) {
-      this.form.place = selectedPlace;
-    },
-    onImageLoaded(newImage) {
-      this.form.image = newImage;
-    },
-    async onSubmit(evt) {
+    async onSubmit(data) {
+      console.log(data);
       try {
-        evt.preventDefault();
-        //After creating if the garment upload image if has it
-        if (this.form.image) {
-          this.form.image = await postImage(this.form.image);
-        }
-        postGarment(this.form);
+        postGarment(data);
         router.replace("/list", () => {
-          this.$root.$bvToast.toast(
-            `Garment ${this.form.name} created`,
-            okToast
-          );
+          this.$root.$bvToast.toast(`Garment ${data.name} created`, okToast);
         });
       } catch (err) {
         this.$bvToast.toast(`Garment could not be created`, errorToast);
       }
-    },
-    onReset(evt) {
-      if (evt) {
-        evt.preventDefault();
-      }
-      this.form.name = "";
-      this.form.garment_type = "";
-      this.form.color = "";
-      this.form.place = "";
-      this.form.status = "";
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
     },
   },
 };
